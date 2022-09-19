9b1b11122b39286af1ce6589659397f5b6f6668b
D'Angelo <96030063+dlo137@users.noreply.github.com>
2022-09-18 20:50:20 -0400

    Add files via upload

---
+++ Music Player/data.js
@@ -0,0 +1,53 @@
+let songs = [
+    {
+        name: 'Amber',
+        path: 'assets/musics/Amber.mp3',
+        artist: '311',
+        cover: 'assets/images/Amber.jpg'
+    },
+    {
+        name: 'Have You Ever Seen the Rain?',
+        path: 'assets/musics/Rain.mp3',
+        artist: 'Creedence Clearwater Revival',
+        cover: 'assets/images/Rain.jpg'
+
+    },
+    {
+        name: 'Positive Vibrations',
+        path: 'assets/musics/positive.mp3',
+        artist: 'Bob Marley',
+        cover: 'assets/images/cover 1.jpg'
+    },
+    {
+        name: 'One Love',
+        path: 'assets/musics/oneLove.mp3',
+        artist: 'Bob Marley',
+        cover: 'assets/images/oneLove.jpg'
+    },
+    {
+        name: 'Come and Get Your Love',
+        path: 'assets/musics/getLove.mp3',
+        artist: 'Redbone',
+        cover: 'assets/images/getLove.jpeg'
+    },
+    {
+        name: 'Turn the Hands of Time',
+        path: 'assets/musics/time.mp3',
+        artist: 'The Intruders',
+        cover: 'assets/images/theIntruders.jpg'
+
+
+    },
+    {
+        name: 'Motorbike',
+        path: 'assets/musics/motorbike.mp3',
+        artist: 'Leon Bridges',
+        cover: 'assets/images/motorbike.jpg'
+    },
+    {
+        name: 'Red Wine',
+        path: 'assets/musics/redWine.mp3',
+        artist: 'UB40',
+        cover: 'assets/images/redWine.jpeg'
+    },
+]
